46c1a2b80af8d112dccbdf855a55b0b6454cd980
Paulo Freitas <42820569+paulofreitas-py@users.noreply.github.com>
2021-04-22 14:55:48 -0300

    Add files via upload

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { Welcome } from "../pages/Welcome";
+import { UserIdentification } from "../pages/UserIdentification";
+import { Confirmation } from "../pages/Confirmation";
+import { plantSelect } from "../pages/PlantSelect";
+
+import colors from "../styles/colors";
+
+const stackRoutes = createStackNavigator();
+
+const AppRoutes: React.FC = () => (
+    <stackRoutes.Navigator
+        headerMode="none"
+        screenOptions={{ cardStyle: { backgroundColor: colors.white } }}
+    >
+        <stackRoutes.Screen name="Welcome" component={Welcome} />
+
+        <stackRoutes.Screen
+            name="UserIdentification"
+            component={UserIdentification}
+        />
+
+        <stackRoutes.Screen name="Confirmation" component={Confirmation} />
+        
+        <stackRoutes.Screen name="PlantSelect" component={plantSelect} />
+    </stackRoutes.Navigator>
+);
+
+export default AppRoutes;
